bf3e56ca2f3cf8d4083a80c3a1fa66976839649a
mfdev-solution <lasolution.mfdev@gmail.com>
2024-05-01 23:23:36 +0000

    creating admin-template and the dashboard home ... component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {LoginComponent} from "./login/login.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {StudentsComponent} from "./students/students.component";
+import {PaymentsComponent} from "./payments/payments.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "home" , component : HomeComponent},
+  {path : "profile" , component : ProfileComponent},
+  {path : "login" , component : LoginComponent},
+  {path : "dashboard" , component : DashboardComponent},
+  {path : "students" , component : StudentsComponent},
+  {path : "payments" , component : PaymentsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuModule} from "@angular/material/menu";
+import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+    DashboardComponent,
+    StudentsComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDrawerContainer,
+    MatSidenavModule,
+    MatNavList,
+    MatListItem,
+    MatDrawerContent
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
